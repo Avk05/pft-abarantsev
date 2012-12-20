6ca871500b2348b96ab7bf4d55195f8cb443d7cf
Alexei Barantsev <barancev@gmail.com>
2012-12-20 19:36:41 +0400

    После первой попыки записать пятое занятие

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,12 +1,5 @@
 package com.example.tests;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.junit.Assert.assertThat;
@@ -16,14 +9,7 @@ import com.example.utils.SortedListOf;
 
 public class GroupCreationTests extends TestBase {
   
-  @DataProvider
-  public Iterator<Object[]> groupFromFile() throws IOException {
-    List<GroupData> groups = GroupDataGenerator.loadGroupsFromXmlFile(new File("groups.xml"));
-    List<Object[]> list = wrapGroupsForDataProvider(groups);
-    return list.iterator();
-  }
-
-  @Test(dataProvider = "groupFromFile")
+  @Test(dataProvider = "randomValidGroupGenerator")
   public void testGroupCreationWithValidData(GroupData group) throws Exception {
     // save old state
     SortedListOf<GroupData> oldList = app.getGroupHelper().getGroups();
